a65c37351b99fd8cefd64a49238da5805a2e004f
MeLlamoGamer <bob878esponja@gmail.com>
2025-02-18 16:05:26 -0300

    Finished installer

--- install.lua
+++ install.lua
@@ -1,2 +1,16 @@
 if pocket then
-	print("")
+	print("Welcome to the PDA_OS installation.")
+	print("The installation process will begin...")
+	print("--------------------------------------")
+	shell.run("mkdir", "os")
+	shell.run("mkdir", "programs")
+	wget https://github.com/MeLlamoGamer/PDA_OS-ComputerCraft/blob/main/startup.lua startup.lua
+	wget https://github.com/MeLlamoGamer/PDA_OS-ComputerCraft/blob/main/.menu .menu
+	wget https://github.com/MeLlamoGamer/PDA_OS-ComputerCraft/blob/main/.back .back
+	wget https://github.com/MeLlamoGamer/PDA_OS-ComputerCraft/blob/main/programs/chat.lua programs/chat.lua
+	wget https://github.com/MeLlamoGamer/PDA_OS-ComputerCraft/blob/main/programs/ftpcli.lua programs/ftpcli.lua
+	wget https://github.com/MeLlamoGamer/PDA_OS-ComputerCraft/blob/main/os/.apps os/.apps
+	wget https://github.com/MeLlamoGamer/PDA_OS-ComputerCraft/blob/main/os/.command os/.command
+	wget https://github.com/MeLlamoGamer/PDA_OS-ComputerCraft/blob/main/os/.power os/.power
+	print("The installation finished successfully, rebooting...")
+	os.reboot()
